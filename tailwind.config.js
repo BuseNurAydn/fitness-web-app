8c85c3abd208ce619313f51126f67fa9223fbc50
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-11-19 22:20:16 +0300

    ilk yükleme

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+
+      },
+      screens:{
+        xs: '480px',
+        sm: '768px',
+        md: '1060px'
+      }
+    },
+  },
+  plugins: [],
+}
+
